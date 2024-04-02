2f85b4bf7d5c675c49bcb9735d6124078dd7cb84
nznznz42 <coolcarrot420@gmail.com>
2024-04-03 02:04:13 +0400

    added copyright

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 nznznz42
 */
 package cmd
 
@@ -18,8 +18,6 @@ var rootCmd = &cobra.Command{
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
